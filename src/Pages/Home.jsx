dd71e1bbceceb95d672f52d749a02e80717a2a91
Abhicoder786 <117636060+abhiCoderShek@users.noreply.github.com>
2024-11-29 17:27:42 +0530

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -28,7 +28,7 @@ export default function Home() {
     
   
     </div>
-    <div className='bg-blue-400  text-white pt-16 pb-16 '>
+    <div className='bg-blue-200  text-white pt-16 pb-16 '>
     <div className=' max-w-6xl m-auto max-md:p-3'>
     <h1 className='text-3xl font-bold'>
       Hire Us for
